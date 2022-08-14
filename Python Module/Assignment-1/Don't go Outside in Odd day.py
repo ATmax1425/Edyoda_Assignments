5a18a1b3f0b9090cd02230245fa26da5fd7505eb
ATmax <atmax1425@gmail.com>
2022-08-14 11:08:34 +0530

    python-module assignment-1 Q-3 update

--- Python Module/Assignment-1/Don't go Outside in Odd day.py
+++ Python Module/Assignment-1/Don't go Outside in Odd day.py
@@ -1,18 +1,11 @@
-print("Enter a value and Press Enter and Put next value at end just Press Enter without a value")
-count = 0
+values_list = list(map(int, input("Enter a values separated by space: ").split()))
 even_count = 0
 odd_count = 0
-while True:
-    value_str = input("Enter value No." + str(count) + ": ")
-    if value_str:
-        value = int(value_str)
-        if value % 2 == 0:
-            even_count += 1
-        else:
-            odd_count += 1
+for i in values_list:
+    if i % 2 == 0:
+        even_count += 1
     else:
-        break
-    count += 1
+        odd_count += 1
 
-print("Number of even numbers: " + str(even_count))
-print("Number of odd numbers: " + str(odd_count))
+print("Number of even days: " + str(even_count))
+print("Number of odd days: " + str(odd_count))
